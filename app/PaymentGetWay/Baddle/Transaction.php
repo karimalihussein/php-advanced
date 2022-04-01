4ca4e2de4d19d8b14d46f4924ef886a794daa00c
karim ali hussein <karimalihussein99@gmail.com>
2022-04-02 01:52:28 +0200

    start with new functions

--- app/PaymentGetWay/Baddle/Transaction.php
+++ app/PaymentGetWay/Baddle/Transaction.php
@@ -3,27 +3,23 @@
 declare(strict_types=1);
 namespace App\PaymentGetWay\Baddle;
 
-use App\Enums\Status;
 
 class Transaction {
 
   
-    private string $status;
 
-    public function __construct(){
-        // self && $this && current class name
-
-        $this->setStatus(Status::PAID);
-        // var_dump(self::STATUS_PAID);
+    public function __construct(
+        public float $amount,
+        public string $status
+    ){
+       
     }
 
-    public function setStatus(string $status): self{
-        if(!isset(Status::ALL_STATUSES[$status])){
-            throw new \InvalidArgumentException('Invalid status');
-        }
-        $this->status = $status;
-        return $this;
+    public function prrcess(){
+        echo "Processing transaction....";
     }
+
+
 
   
     
