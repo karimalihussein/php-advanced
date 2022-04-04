837158df6f9683c19f0d5bcd964e60bcd6883722
karim ali hussein <karimalihussein99@gmail.com>
2022-04-04 22:32:32 +0200

    update invock method

--- app/Invoice.php
+++ app/Invoice.php
@@ -1,22 +1,19 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App;
 
 class Invoice {
 
-    protected function process(float $amount, $description){
-        var_dump($amount, $description);
-    }
 
-     public function __call(string $name,array $arguments){
-         if(method_exists($this, $name)){
-            call_user_func_array([$this,$name],$arguments);
-         }
-     }
+    // public function __toString(): string {
+    //       return "karim ali hussein";
+    // }
 
-     public static function __callStatic(string $name, $arguments){
-         var_dump($name, $arguments);
-     }
+    // public function __invoke(){
+    //     var_dump('invoiced');
+    // }
 
+          
 }
--- public/index.php
+++ public/index.php
@@ -7,8 +7,13 @@ declare(strict_types = 1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new App\Invoice();
+// $invoice();
+var_dump(is_callable($invoice));
+$invoice();
+// var_dump($invoice instanceof Stringable);
+// echo $invoice; 
 // App\Invoice::process(1,2,3);
-$invoice->process(15,'karim ali hussein');
+// $invoice->process(15,'karim ali hussein');
 // var_dump(isset($invoice->amount));
 // var_dump($invoice);
 // echo $invoice->amount . PHP_EOL;
